1049998337ea81a96969446893718e5c10603775
codeCrafter10-alt <aaravg1410@gmail.com>
2026-03-18 23:27:28 -0700

    Add backprop method to Node

--- nn/node.py
+++ nn/node.py
@@ -10,5 +10,10 @@ class Node:
         Accumulated during backpropagation
     """
     def __init__(self, value):
+        # Initialize node with a value and zero gradient
         self.data = value
         self.grad = 0
+
+    def backprop(self, incoming_grad):
+        # Accumulate incoming gradient during backpropagation
+        self.grad += incoming_grad
