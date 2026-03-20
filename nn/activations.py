8dd7b9299d110c8771826881a23dc17c554b6a65
codeCrafter10-alt <aaravg1410@gmail.com>
2026-03-19 20:39:05 -0700

    Add activations.py file

---
+++ nn/activations.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class ReLU:
+    def forward(self, x):
+        pass
+
+    def backprop(self, incoming_grad):
+        pass
+
+class Softmax:
+    def forward(self, x):
+        pass
+
+    def backprop(self, incoming_grad):
+        pass
